6bdfd55dbccdc8b5889898e25d28a00a11522741
Maciej Winnicki <maciej.winnicki@gmail.com>
2014-03-02 09:58:24 +0100

    Indent with 2 spaces

--- test/agent.js
+++ test/agent.js
@@ -5,52 +5,56 @@ var EventEmitter = require('events').EventEmitter;
 var Agent = require('../lib/agent');
 
 describe('Agent', function () {
-    beforeEach(function () {
-        this.socket = new EventEmitter();
-        this.socketSpy = sinon.spy(this.socket, 'emit');
+  beforeEach(function () {
+    this.socket = new EventEmitter();
+    this.socketSpy = sinon.spy(this.socket, 'emit');
+  });
+
+  it('should send job to remote agent using socket', function () {
+    var agent = new Agent(this.socket);
+
+    agent.work(function () { console.log('test'); });
+
+    this.socketSpy.lastCall.args[0].should.eql('work');
+    this.socketSpy.lastCall.args[1].executor.should.eql('function () { console.log(\'test\'); }');
+  });
+
+  it('should send data to remote agent', function () {
+    var agent = new Agent(this.socket);
+
+    agent.work(function () { console.log('test'); }, {user: 1});
+
+    this.socketSpy.lastCall.args[1].should.eql({
+      executor: 'function () { console.log(\'test\'); }',
+      data: { user: 1 }
     });
+  });
 
-    it('should send job to remote agent using socket', function () {
-        var agent = new Agent(this.socket);
+  it('should (un)register callback when remote agent is done', function (done) {
+    var agent = new Agent(this.socket);
 
-        agent.work(function () { console.log('test'); });
+    agent.work(
+      function () {
+        console.log('test');
+      },
+      {},
+      done
+    );
 
-        this.socketSpy.lastCall.args[0].should.eql('work');
-        this.socketSpy.lastCall.args[1].executor.should.eql('function () { console.log(\'test\'); }');
-    });
+    this.socket.emit('done');
+    this.socket.emit('done');
+  });
 
-    it('should send data to remote agent', function () {
-        var agent = new Agent(this.socket);
+  it('should optionally accept callback as a second parameter', function (done) {
+    var agent = new Agent(this.socket);
 
-        agent.work(function () { console.log('test'); }, {user: 1});
+    agent.work(
+      function () {
+        console.log('test');
+      },
+      done
+    );
 
-        this.socketSpy.lastCall.args[1].should.eql({
-            executor: 'function () { console.log(\'test\'); }',
-            data: { user: 1 }
-        });
-    });
-
-    it('should (un)register callback when remote agent is done', function (done) {
-        var agent = new Agent(this.socket);
-
-        agent.work(
-            function () { console.log('test'); },
-            {},
-            done
-        );
-
-        this.socket.emit('done');
-        this.socket.emit('done');
-    });
-
-    it('should optionally accept callback as a second parameter', function (done) {
-        var agent = new Agent(this.socket);
-
-        agent.work(
-            function () { console.log('test'); },
-            done
-        );
-
-        this.socket.emit('done');
-    });
+    this.socket.emit('done');
+  });
 });
--- test/mistrz.js
+++ test/mistrz.js
@@ -6,38 +6,39 @@ var BrowserScript = require('../lib/browser_script');
 var EventEmitter = require('events').EventEmitter;
 
 describe('mistrz', function () {
-    beforeEach(function () {
-        this.namespace = new EventEmitter();
-        this.namespaceStub = sinon.stub(this.namespace, 'on').withArgs('connection').yieldsAsync();
+  beforeEach(function () {
+    this.namespace = new EventEmitter();
+    this.namespaceStub = sinon.stub(this.namespace, 'on').withArgs('connection').yieldsAsync();
 
-        this.io = {of: function () {}};
-        this.ioStub = sinon.stub(this.io, 'of').withArgs('/mistrz').returns(this.namespace);
+    this.io = {of: function () {
+    }};
+    this.ioStub = sinon.stub(this.io, 'of').withArgs('/mistrz').returns(this.namespace);
+  });
+
+  it('should emit "new" event for every connected agent', function (done) {
+    var mistrz = require('../')(this.io);
+    mistrz.on('new', function () {
+      done();
     });
 
-    it('should emit "new" event for every connected agent', function (done) {
-        var mistrz = require('../')(this.io);
-        mistrz.on('new', function () {
-            done();
-        });
+    this.ioStub.called.should.equal(true);
+    this.namespaceStub.called.should.equal(true);
+  });
 
-        this.ioStub.called.should.equal(true);
-        this.namespaceStub.called.should.equal(true);
+  it('should emit "new" event with agent instance', function (done) {
+    var mistrz = require('../')(this.io);
+    mistrz.on('new', function (agent) {
+      agent.should.be.instanceOf(Agent);
+      done();
     });
 
-    it('should emit "new" event with agent instance', function (done) {
-        var mistrz = require('../')(this.io);
-        mistrz.on('new', function (agent) {
-            agent.should.be.instanceOf(Agent);
-            done();
-        });
+    this.ioStub.called.should.equal(true);
+    this.namespaceStub.called.should.equal(true);
+  });
 
-        this.ioStub.called.should.equal(true);
-        this.namespaceStub.called.should.equal(true);
-    });
+  it('should return browser script', function () {
+    var mistrz = require('../')(this.io);
 
-    it('should return browser script', function () {
-        var mistrz = require('../')(this.io);
-
-        mistrz.script.should.be.an.instanceOf(BrowserScript);
-    });
+    mistrz.script.should.be.an.instanceOf(BrowserScript);
+  });
 });
