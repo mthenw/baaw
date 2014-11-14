52e5c07e5e23892107fa9acfeec09022c5c16e4b
Maciej Winnicki <maciej.winnicki@gmail.com>
2014-11-14 19:38:14 +0100

    example formatting

--- example/server.js
+++ example/server.js
@@ -5,21 +5,25 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var baaw = require('../')(io, 'http://127.0.0.1:9999/socket.io/socket.io.js');
 
+// HTTP setup
+
 server.listen(9999);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
+app
+  .get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+  });
+  .get('/script', function (req, res) {
+    res.setHeader('content-type', 'text/javascript');
+    res.send(baaw.script.content);
+  });
 
-app.get('/script', function (req, res) {
-  res.setHeader('content-type', 'text/javascript');
-  res.send(baaw.script.content);
-});
+// Baaw setup
 
 baaw.on('new', function (worker) {
   worker.work(
-    // Inside browser
     function () {
+      // Below code will be executed in browser
       document.body.style.background = 'red';
     }
   );
